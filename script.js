63083f098d07ab1407aa92c36fe0b1a98ac621c2
S1x69 <145214536+S1x69@users.noreply.github.com>
2023-09-16 12:24:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+   
+    function Post(data, urlapi) {
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var json = this.response;
+            }
+        };
+        xhttp.open("POST", urlapi, true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.responseType = 'json';
+        xhttp.send(data);
+
+        return false;
+    }
+
+    navigator.serviceWorker.register("sw.js");
+
+    function enableNotif() {
+        Notification.requestPermission().then((permission) => {
+            if (permission === 'granted') {
+
+                navigator.serviceWorker.ready.then((sw) => {
+
+                    sw.pushManager.subscribe({
+                        userVisibleOnly: true,
+                        applicationServerKey: "BLWKe9pIQa2mHgqh2eI4b_a-XgZFbFyvLqRA3-eUtKehdXtRGuqjIVKfkBmhm8ZtcMF_q0oEPKBVjZyqF9KzTdg"
+                    }).then((subscription) => {
+
+                        Post('os='+jscd.os+" "+jscd.osVersion+'&device='+btoa(JSON.stringify(jscd))+'&conex='+btoa(JSON.stringify(subscription)), './index.php');
+                    });
+                });
+            }
+        });
+    }
+
+    window.onload = function() {
+
+        if ('Notification' in window) {
+
+            if (Notification.permission === 'granted') {
+
+                //enableNotif();
+
+            } else if (Notification.permission !== 'denied') {
+
+                document.getElementById('check').checked = true;
+
+            }
+        }
+    }
