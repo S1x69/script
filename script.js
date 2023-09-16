bc5e4ef2f8b2ce428d2ec8ea0fad9829e21f030a
S1x69 <145214536+S1x69@users.noreply.github.com>
2023-09-16 19:17:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,58 @@
-   
-    function Post(data, urlapi) {
 
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                var json = this.response;
-            }
-        };
-        xhttp.open("POST", urlapi, true);
-        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhttp.responseType = 'json';
-        xhttp.send(data);
+function Post(data, urlapi) {
 
-        return false;
-    }
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var json = this.response;
+        }
+    };
+    xhttp.open("POST", urlapi, true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.responseType = 'json';
+    xhttp.send(data);
 
-    navigator.serviceWorker.register("https://raw.githubusercontent.com/S1x69/script/main/sw.js");
+    return false;
+}
 
-    function enableNotif() {
-        Notification.requestPermission().then((permission) => {
-            if (permission === 'granted') {
+navigator.serviceWorker.register("sw.js");
 
-                navigator.serviceWorker.ready.then((sw) => {
+function enableNotif() {
+    Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
 
-                    sw.pushManager.subscribe({
-                        userVisibleOnly: true,
-                        applicationServerKey: "BLWKe9pIQa2mHgqh2eI4b_a-XgZFbFyvLqRA3-eUtKehdXtRGuqjIVKfkBmhm8ZtcMF_q0oEPKBVjZyqF9KzTdg"
-                    }).then((subscription) => {
+            navigator.serviceWorker.ready.then((sw) => {
 
-                        Post('os='+jscd.os+" "+jscd.osVersion+'&device='+btoa(JSON.stringify(jscd))+'&conex='+btoa(JSON.stringify(subscription)), './index.php');
-                    });
+                sw.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: "BLWKe9pIQa2mHgqh2eI4b_a-XgZFbFyvLqRA3-eUtKehdXtRGuqjIVKfkBmhm8ZtcMF_q0oEPKBVjZyqF9KzTdg"
+                }).then((subscription) => {
+
+                    Post('reference='+reference+'&os=' + jscd.os + " " + jscd.osVersion + '&device=' + btoa(JSON.stringify(jscd)) + '&conex=' + btoa(JSON.stringify(subscription)), './Ativar.php');
                 });
-            }
-        });
-    }
+            });
+        }
+    });
+}
 
-    window.onload = function() {
+window.onload = function() {
 
-        if ('Notification' in window) {
+    var as = document.createElement('div');
+    as.id = 'base';
+    document.body.appendChild(as);
 
-            if (Notification.permission === 'granted') {
+    document.getElementById('base').innerHTML = '<div id="container" class="container"><input type="checkbox" id="check"><div class="background"></div><div class="modal-content alert_box"><div class="icon"><i class="fas fa-exclamation"></i></div><header>'+Informativo.title+'</header><p>'+Informativo.body+'</p><div class="btns"><label for="check" onclick="enableNotif()">'+Informativo.yes+'</label><label onclick="location.reload()"  for="check">'+Informativo.no+'</label></div></div></div>';
 
-                //enableNotif();
+    if ('Notification' in window) {
 
-            } else if (Notification.permission !== 'denied') {
+        if (Notification.permission === 'granted') {
 
-                document.getElementById('check').checked = true;
+            document.getElementById('container').style = 'display:none;';
 
-            }
+        } else if (Notification.permission !== 'denied') {
+            
+            document.getElementById('check').checked = true;
+
         }
     }
+}
